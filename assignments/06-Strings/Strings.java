407f958136847256863d99dade25e24ef61b08f8
rupesh <rupeshkuraganti@gmail.com>
2023-12-12 16:10:32 +0530

    12/12/23

---
+++ assignments/06-Strings/Strings.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Strings {
+    public static void main(String[] args) {
+        String str = "notebook";
+        leetcode1704(str);
+    }
+
+    static void leetcode1704(String str){
+        int count=0;
+        String vowels="aeiouAEIOU";
+        for (int i = 0; i < str.length(); i++) {
+            if(i<str.length()/2) {
+                if (vowels.contains(str.charAt(i) + "")) ;
+                count++;
+            }else {
+                if (vowels.contains(str.charAt(i) + "")) ;
+                count--;
+            }
+        }
+        System.out.println(count==0);
+    }
+
+
+}
