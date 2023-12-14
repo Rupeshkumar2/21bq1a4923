b5999b99a506582920142e4d720915b983ec1450
rupesh <rupeshkuraganti@gmail.com>
2023-12-14 15:52:22 +0530

    14/12/23

--- assignments/06-Strings/Strings.java
+++ assignments/06-Strings/Strings.java
@@ -6,19 +6,19 @@ public class Strings {
         leetcode1704(str);
     }
 
-    static void leetcode1704(String str){
-        int count=0;
-        String vowels="aeiouAEIOU";
-        for (int i = 0; i < str.length(); i++) {
-            if(i<str.length()/2) {
-                if (vowels.contains(str.charAt(i) + "")) ;
+    static void leetcode1704(String str) {
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+        for (int i = 0 ; i < str.length() ; i++) {
+            if (i < str.length() / 2) {
+                if (vowels.contains(str.charAt(i) + ""));
                 count++;
-            }else {
-                if (vowels.contains(str.charAt(i) + "")) ;
+            } else {
+                if (vowels.contains(str.charAt(i) + ""));
                 count--;
             }
         }
-        System.out.println(count==0);
+        System.out.println(count == 0);
     }
 
 
